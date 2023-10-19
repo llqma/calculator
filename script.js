a23996013e29514b1234805f4d1a867f0fc2e0f5
Li 丽 <lisalams@live.se>
2023-10-19 13:16:52 +0200

    Create variables in JS file

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelector('#number-button');
+const operationButtons = document.querySelector('#operation-button');
+const equalsButton = document.querySelector('#equals-button');
+const clearButton = document.querySelector('#clear-button');
+const deleteButton = document.querySelector('#delete-button');
+const percentButton = document.querySelector('#percent-button');
+const screenDisplay = document.querySelector('#display');
